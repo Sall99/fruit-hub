e42e157b11f914db15c395b02fd60da2236cdc03
Sall <sallaboudaouda@gmail.com>
2024-10-19 10:33:43 +0000

    first commit

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,47 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
+  },
+
+  orange: {
+    text: "#F08626",
+    background: "#FFA451",
+    icon: "#FFA451",
+    light: "#FEF0F0",
+  },
+  blue: {
+    darkText: "#27214D",
+    lightText: "#5D577E",
+  },
+  gray: {
+    background: "#F3F1F1",
+    light: "#F1EFF6",
+  },
+  yelow: {
+    light: "#F1EFF6",
+  },
+
+  green: {
+    backgroud: "#4CD964",
+    light: "#E0FFE5",
   },
 };
