37a4fa55ce926cfdfa6f0e29fdd0ad62c2ae657e
Sall <sallaboudaouda@gmail.com>
2024-10-19 11:00:30 +0000

    added fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    BrandonGrotesqueLight: require("../assets/fonts/Brandon_Grotesque_light.otf"),
+    BrandonGrotesqueRegular: require("../assets/fonts/Brandon-Grotesque-regular.otf"),
+    BrandonGrotesqueMedium: require("../assets/fonts/Brandon_Grotesque_medium.otf"),
+
+    BadScriptRegular: require("../assets/fonts/BadScript-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" />
